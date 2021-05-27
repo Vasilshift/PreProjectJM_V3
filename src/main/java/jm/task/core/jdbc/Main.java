20a6b5a1472c28aa5614bdf15777074df9d35bca
Evgeny <vasilkalin@gmail.com>
2021-05-27 22:57:19 +0200

    Don't add table

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,65 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 import java.sql.*;
 
 public class Main {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
-    private static final String USERNAME = "evgen";
-    private static final String PASSWORD = "linline012";
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-            //Connection connection;
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-            try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
-
-                //Driver driver = new com.mysql.jdbc.Driver();
-                System.out.println("driver downloaded");
-                System.out.println("driver manager runned");
-
-                Statement statement = connection.createStatement();
-
-                statement.addBatch("DROP TABLE IF EXISTS `test`.`users`;");
-                statement.addBatch("CREATE TABLE `test`.`users` (`id` INT NOT NULL, " +
-                        " `name` VARCHAR(45) NOT NULL," +
-                        " `lastName` VARCHAR(45) NOT NULL," +
-                        " `age` INT(3) NOT NULL)" +
-                        //" PRIMARY KEY (`id`)," +
-                        //" UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)" +
-                        ";"
-                );
-                statement.addBatch(
-                        "insert into `test`.`users` (id, name, lastName, age) values (1, 'Misha', 'Vasilev', 34);"
-                );
-                System.out.printf("user с именем \s% добавлен в БД", );
-                statement.addBatch(
-                        "insert into `test`.`users` (id, name, lastName, age) values (1, 'Sasha', 'Romanov', 65);"
-                );
-                statement.addBatch(
-                        "insert into `test`.`users` (id, name, lastName, age) values (1, 'Alex', 'Porshnev', 18);"
-                );
-                statement.addBatch(
-                        "insert into `test`.`users` (id, name, lastName, age) values (1, 'Tim', 'Bird', 52);"
-                );
-
-                //statement.addBatch("DROP TABLE `test`.`users`;");
+//        try {
+            Util conn = new Util();
+            conn.UtilConnect();
+            UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+            //userDaoJDBC.saveUser("gasdg", "dgdfg", (byte) 56);
+            userDaoJDBC.dropUsersTable();
+//            userDaoJDBC.createUsersTable();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        //conn.UtilConnect();
+        //userDaoJDBC.saveUser("Midfa", "Likes", (byte) 45);
 
 
 
 
 
-                statement.executeBatch();
-                System.out.println("connection runned");
-                if(!connection.isClosed()) {
-                    System.out.println("Connection done");
-                }
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.err.println("Неудалось загрузить класс драйвера");
-            }
+    }
 
-        }
+
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,25 +1,66 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection connection;
+
     public UserDaoJDBCImpl() {
-
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
+        //Connection connection = null;
 
+        try {
+            Util conn = new Util();
+            conn.UtilConnect();
+            Statement statement = connection.createStatement();
+            statement.addBatch("DROP TABLE IF EXISTS `test`.`users`;");
+            statement.addBatch("CREATE TABLE `test`.`users` (`id` INT NOT NULL, " +
+                    " `name` VARCHAR(45) NOT NULL," +
+                    " `lastName` VARCHAR(45) NOT NULL," +
+                    " `age` INT(3) NOT NULL)" +
+                    //" PRIMARY KEY (`id`)," +
+                    //" UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)" +
+                    ";"
+            );
+            //statement.executeBatch();
+        } catch (Exception e) {}
+    }
+    public void dropUsersTable() throws SQLException {
+        try {
+//            Util conn = new Util();
+//            conn.UtilConnect();
+            Statement statement = connection.createStatement();
+            statement.addBatch("DROP TABLE `test`.`users`;");
+            statement.executeBatch();
+        } catch (Exception e) {}
     }
 
-    public void dropUsersTable() {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
 
+        try {
+            Util conn = new Util();
+            conn.UtilConnect();
+            Statement statement = connection.createStatement();
+            statement.execute("insert into `test`.`users` (id, name, lastName, age) values (5, 'ty', 'fg', 56);");
+
+//            statement.addBatch(
+//                    "insert into `test`.`users` (id," + name + ", lastName, age) values (5, 'ty', 'fg', 56);"
+//            );
+//            statement.executeBatch();
+            System.out.printf("user с именем %s добавлен в БД", "Misha");
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing sql:\n", e);
+        }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
-    }
 
     public void removeUserById(long id) {
 
